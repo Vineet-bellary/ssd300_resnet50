44a8c3ffe125eed51a5c48e81e58b9e3094a5d3e
Vineet-bellary <vineet.bellary@gmail.com>
2026-02-01 17:54:17 +0530

    SSD300

--- new_backbone.py
+++ new_backbone.py
@@ -1,17 +1,18 @@
 from torchvision.models import resnet50, ResNet50_Weights
 import torch.nn as nn
 
+
 class ResNet50Backbone(nn.Module):
     def __init__(self):
         super().__init__()
         # Using the latest weights parameter
         backbone = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
-        
-        # Layer 1-2: Output size (B, 512, 28, 28) for 224x224 input
-        self.stage1 = nn.Sequential(*list(backbone.children())[:6]) 
-        # Layer 3: Output size (B, 1024, 14, 14)
+
+        # Layer 1-2: Output size (B, 512, 38, 38) for 300x300 input
+        self.stage1 = nn.Sequential(*list(backbone.children())[:6])
+        # Layer 3: Output size (B, 1024, 19, 19)
         self.stage2 = nn.Sequential(*list(backbone.children())[6:7])
-        # Layer 4: Output size (B, 2048, 7, 7)
+        # Layer 4: Output size (B, 2048, 10, 10)
         self.stage3 = nn.Sequential(*list(backbone.children())[7:8])
 
     def forward(self, x):
